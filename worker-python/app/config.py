041926f2e24553fa19112b3fd2f854491a2788c7
rajaNayak123 <nayakraja151@gmail.com>
2026-06-21 08:29:30 +0530

    config(worker): add INTERNAL_TOKEN environment variable config

--- worker-python/app/config.py
+++ worker-python/app/config.py
@@ -15,6 +15,8 @@ WORKER_PORT = int(os.getenv("WORKER_PORT", "8000"))
 
 SPRING_BACKEND_URL = os.getenv("SPRING_BACKEND_URL", "http://backend:8080")
 
+INTERNAL_TOKEN = os.getenv("INTERNAL_TOKEN", "default-internal-token-secret")
+
 MAX_RETRY_ATTEMPTS = int(os.getenv("MAX_RETRY_ATTEMPTS", "3"))
 
 BASE_RETRY_DELAY_S = float(os.getenv("BASE_RETRY_DELAY_S", "2.0"))
